6d0f6adb05381d0f2dc271ad080f0aecdcaa3ae9
RamiRbehi <ramirbehi92@gmail.com>
2021-10-05 21:42:35 +0100

    temognage 2

--- src/Components/Témoignages.jsx
+++ src/Components/Témoignages.jsx
@@ -8,17 +8,17 @@ import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+// const style = {
+//     position: 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: 400,
+//     bgcolor: 'background.paper',
+//     border: '2px solid #000',
+//     boxShadow: 24,
+//     p: 4,
+//   };
 
 function Témoignages(props) {
 
